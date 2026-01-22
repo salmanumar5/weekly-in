b635e7468d89589306a819b170a5443656fc70e9
Umar Salman <umarsalman555@gmail.com>
2026-01-22 11:14:25 +0530

    small fix for import vars

--- api/tracker.ts
+++ api/tracker.ts
@@ -1,7 +1,7 @@
 
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { getDb } from '../lib/db';
-import { Status } from '../types';
+import { getDb } from '../lib/db.js';
+import { Status } from '../types.js';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   const db = await getDb();
